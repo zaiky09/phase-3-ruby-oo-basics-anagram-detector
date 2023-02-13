9bc74fbb90df463a309088be2eacf638702360ed
zaiky09 <zamilmozamil@outlook.com>
2023-02-13 14:22:39 +0300

    Anagram done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+      @word = word.downcase
+    end
+  
+    def match(word_list)
+      word_list.select { |wrd| wrd.downcase.chars.sort == @word.chars.sort && wrd.downcase != @word }
+    end
+end
